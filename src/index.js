325603c4a33ce35db5fb140aed40cbb1177f632f
Carlton Joseph <carlton.joseph@gmail.com>
2018-07-18 13:20:56 -0700

    add redux dev tools

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import { createEpicMiddleware } from "redux-observable";
 
@@ -12,7 +12,12 @@ import { rootEpic } from "./epics";
 
 const epicMiddleware = createEpicMiddleware();
 
-const store = createStore(reducer, applyMiddleware(epicMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(epicMiddleware))
+);
 epicMiddleware.run(rootEpic);
 
 ReactDOM.render(
