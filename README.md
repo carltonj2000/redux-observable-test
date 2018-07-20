66af6544cde6daf7d9fba3a700e5a8fcf5a3900c
Carlton Joseph <carlton.joseph@gmail.com>
2018-07-19 18:38:48 -0700

    add ajax calls

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Up and Running with redux-observable
 
-The code in this repository is based on the  
+The code in this repository is based on the
 [Up and Running with redux-observable](https://egghead.io/courses/up-and-running-with-redux-observable)
 course.
--- src/epics/index.js
+++ src/epics/index.js
@@ -1,41 +1,25 @@
-import { of } from "rxjs";
-import { switchMap, delay } from "rxjs/operators";
+import { map, mergeMap, switchMap } from "rxjs/operators";
+import { forkJoin } from "rxjs";
+import { ajax } from "rxjs/ajax";
 import { combineEpics } from "redux-observable";
 
 import { LOADSTORIES, loadStoriesDone } from "../actions";
-const stories = [
-  {
-    by: "bleakgadfly",
-    id: 14967192,
-    title: "To Protect Voting, Use Open-Source Software",
-    url:
-      "https://mobile.nytimes.com/2017/08/03/opinion/open-source-software-hacker-voting.html"
-  },
-  {
-    by: "mtyurt",
-    id: 14966545,
-    title: "Git: Using Advanced Rebase Features for a Clean Repository",
-    url:
-      "https://mtyurt.net/2017/08/08/git-using-advanced-rebase-features-for-a-clean-repository/"
-  },
-  {
-    by: "callumlocke",
-    id: 14967335,
-    title: "Inside an AI brain: What does machine learning look like?",
-    url: "https://www.graphcore.ai/posts/what-does-machine-learning-look-like"
-  }
-];
-/*
 const topStories = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`;
 const url = id =>
   `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`;
-*/
+
 const loadStoriesEpic = actions$ => {
-  return actions$
-    .ofType(LOADSTORIES)
-    .pipe(
-      switchMap(({ payload }) => of(loadStoriesDone(stories)).pipe(delay(2000)))
-    );
+  return actions$.ofType(LOADSTORIES).pipe(
+    switchMap(({ payload }) =>
+      ajax.getJSON(topStories).pipe(
+        map(ids => ids.slice(0, 5)),
+        map(ids => ids.map(url)),
+        map(urls => urls.map(url => ajax.getJSON(url))),
+        mergeMap(reqs => forkJoin(reqs)),
+        map(stories => loadStoriesDone(stories))
+      )
+    )
+  );
 };
 
 export const rootEpic = combineEpics(loadStoriesEpic);
